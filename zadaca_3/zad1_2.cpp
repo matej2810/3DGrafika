970b29759c51ce300fa7a9ef7961f689eb5f151a
Matej Jurec <73646988+matej2810@users.noreply.github.com>
2023-01-06 16:26:03 +0100

    Dodana dz

---
+++ zadaca_3/zad1_2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "tgaimage.h"
+using namespace std;
+
+// dimenzije slike
+const int width  = 512;
+const int height = 512;
+
+// definirajmo boje
+const TGAColor green = TGAColor(0, 255, 0, 1);
+const TGAColor red   = TGAColor(255, 0, 0, 1);
+const TGAColor blue  = TGAColor(0, 0, 255, 1);
+               
+
+//pomocna funkcija za racunanje
+float fja(float xa,float ya,float xc,float yc,float x,float y) {
+
+    return ((ya-yc)*x+(xc-xa)*y+xa*yc-xc*ya);
+}
+
+
+void draw_triangle_2d(float x0, float y0, float x1, float y1, float x2, float y2 ,TGAImage &image,TGAColor color) {   
+
+    int maxX = ceil (max (max(x0,x1), x2));
+    int minX = floor (min (min(x0,x1), x2));
+
+    int maxY = ceil (max (max(y0,y1), y2));
+    int minY = floor(min (min(y0,y1), y2));
+   
+    float alpha,beta,gama;
+
+    for (int j =minY; j <= maxY ; ++j){
+        for (int i = minX; i <= maxX; ++i){
+            
+            alpha = fja(x1,y1,x2,y2,i,j)/fja(x1,y1,x2,y2,x0,y0); 
+            beta= fja(x2,y2,x0,y0,i,j)/fja(x2,y2,x0,y0,x1,y1);  
+            gama= fja(x0,y0,x1,y1,i,j)/fja(x0,y0,x1,y1,x2,y2);  
+               
+            
+            if (alpha >= 0 && alpha <= 1 && beta >= 0 && beta <= 1 && gama >= 0 && gama <= 1) 
+            { 
+                if(( alpha > 0 ||((fja(x1,y1,x2,y2,x0,y0))*(fja(x1,y1,x2,y2,-1,-1))) >=0)
+                 &&( beta > 0 || ((fja(x2,y2,x0,y0,x1,y1))*(fja(x2,y2,x0,y0,-1,-1))) >=0)
+                 &&( gama > 0 || (fja(x0,y0,x1,y1,x2,y2))*(fja(x0,y0,x1,y1,-1,-1))) >=0)
+                 {
+                        image.set(j, i, color);
+                }  
+            }
+        }
+    } 
+}
+
+
+
+void draw_triangle_2d_gouraurd (float x0, float y0, float x1, float y1, float x2, float y2 ,TGAImage &image,TGAColor c0,TGAColor c1,TGAColor c2){
+    
+    int maxX = ceil (max (max(x0,x1), x2));
+    int minX = floor (min (min(x0,x1), x2));
+
+    int maxY = ceil (max (max(y0,y1), y2));
+    int minY = floor(min (min(y0,y1), y2));
+   
+    float alpha,beta,gama;
+    TGAColor color;
+
+    for (int j = minY; j <= maxY; ++j){
+        for (int i = minX; i <= maxX; ++i){
+
+            float f1 = fja(x1, y1, x2, y2, x0, y0);
+            float f2 = fja(x2, y2, x0, y0, x1, y1);
+            float f3 = fja(x0, y0, x1, y1, x2, y2);
+            
+            alpha= fja(x1,y1,x2,y2,i,j)/f1; 
+            beta= fja(x2,y2,x0,y0,i,j)/f2;  
+            gama= fja(x0,y0,x1,y1,i,j)/f3;  
+               
+            
+            if (alpha >= 0 && alpha <= 1 && beta >= 0 && beta <= 1 && gama >= 0 && gama <= 1)
+            {              
+                if ((alpha>0 ||(f1 * (fja(x1,y1,x2,y2,-1,-1))) >=0)
+                 && (beta>0 || (f2 * (fja(x2,y2,x0,y0,-1,-1))) >=0)
+                 && (gama>0 || (f3 * (fja(x0,y0,x1,y1,-1,-1))) >=0))
+                 {
+                        color.r=alpha*c0.r+beta*c1.r+gama*c2.r;
+                        color.g=alpha*c0.g+beta*c1.g+gama*c2.g;
+                        color.b=alpha*c0.b+beta*c1.b+gama*c2.b;
+                        image.set(j, i, color);
+                }  
+            }
+        }
+    } 
+}
+
+int main()
+{
+    // definiraj sliku
+    TGAImage image(width, height, TGAImage::RGB);
+    
+    // nacrtaj trokut 
+    draw_triangle_2d(20, 40, 180, 150, 100,210,image, blue);
+    draw_triangle_2d(20, 200, 180, 250, 100, 340,image, green);
+    draw_triangle_2d_gouraurd(250, 250, 250, 440, 440, 440, image, green, blue, red);
+
+    // spremi sliku 
+    image.flip_vertically();
+    image.write_tga_file("zadatak1_2.tga");
+}
---
+++ zadaca_3/zad3.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "tgaimage.h"
+using namespace std;
+
+
+// dimenzije slike
+const int width  = 512;
+const int height = 512;
+
+// definirajmo boje
+const TGAColor green = TGAColor(0, 255, 0, 1);
+const TGAColor red   = TGAColor(255, 0, 0, 1);
+const TGAColor blue  = TGAColor(0, 0, 255, 1);
+               
+typedef float Vec3[3]; 
+typedef float Vec2[2];
+
+//pomocna funkcija za racunanje
+float fja(const Vec3 &a, const Vec3 &b, const Vec3 &c) { 
+    return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]); 
+} 
+
+
+void draw_triangle(float x0, float y0,float z0, float x1, float y1,float z1, float x2, float y2,float z2 ,TGAImage &image,TGAColor color)
+{   
+    Vec3 a1 = {x0,y0,z0};
+    Vec3 a2 = {x1,y1,z1};
+    Vec3 a3 = {x2, y2,z2};
+
+    a1[0] /= a1[2]; 
+    a1[1] /= a1[2];
+
+    a2[0] /= a2[2]; 
+    a2[1] /= a2[2];
+
+    a3[0] /= a3[2]; 
+    a3[1] /= a3[2];
+    
+    a1[1]= (1 + a1[1]) * 0.5 * height; 
+    a1[0] = (1 + a1[0]) * 0.5 * width; 
+    
+    a2[1]= (1 + a2[1]) * 0.5 * height; 
+    a2[0] = (1 + a2[0]) * 0.5 * width; 
+
+    a3[1]= (1 + a3[1]) * 0.5 * height; 
+    a3[0] = (1 + a3[0]) * 0.5 * width; 
+    
+    a1[2] = 1 / a1[2];  
+    a2[2]= 1 / a2[2]; 
+    a3[2] = 1 / a3[2];
+
+    for (int j =0; j<height ; j++)
+    {
+        for (int i= 0;i<width;i++)
+        {
+            float p1 = (float)i+0.5f;
+            float p2 =  height-(float)j+0.5f;
+            Vec3 p = {p1, p2, 0};
+            float alpha = fja(a3,a2,p); 
+            float beta = fja(a1,a3,p);
+            float gama = fja(a2,a1,p); 
+
+            if((alpha >= 0 && beta >= 0 && gama >= 0) ) 
+            { 
+                image.set(j,i,color);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    // definiraj sliku
+    TGAImage image(width, height, TGAImage::RGB);
+    
+    // nacrtaj trokut 
+    draw_triangle(-48,-10,82,29,-15,44,13,34,114,image,blue);
+    
+    // spremi sliku 
+    image.flip_vertically();
+    image.write_tga_file("zadatak3.tga");
+}
